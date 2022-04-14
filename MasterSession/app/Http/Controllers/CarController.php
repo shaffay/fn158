26f02762eb3da2e0f8c8c2f381c94e69111fbf73
Shaffay Bajwa <Bajwa@aptechtr.edu>
2022-04-14 16:35:56 +0500

    Master Hai 14/April/22
    Yd rakhna practice krnwa

---
+++ MasterSession/app/Http/Controllers/CarController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $cars = car::all();
+
+        
+        return view('cars.index',compact('cars'));
+        //return $cars;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return "Cars Create Page";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(car $car)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(car $car)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, car $car)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(car $car)
+    {
+        //
+    }
+}
